7a31e2dd719d8239643d3cbaf41ba683009c81b1
simon stoneman <simon.stoneman@yahoo.co.uk>
2022-12-06 22:16:41 +0000

    with the previous way of cycling through the q's with for loop, i could not get the program to wait for user input and validation before moving on due to async. After head scratching and googling, decided I could recursivly call the addQuestion func to cycle through 1 by 1. was now trying to clear the feedback area before each new q, but its pushing my buttons at mo. Submitting what I have, but will finish it...

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -20,6 +20,8 @@
 var timerStart = 100;
 var wrongAnsDeduc = 10;
 var startScreenBlk = document.querySelector('#start-screen');
+var feedbackBlk = document.querySelector('#feedback');
+var questionsListLgth = questionsList.length;
 var currentTime = 0;
 var currentQuestionIndex = 0;
 ////////////Global Vars End
@@ -29,18 +31,18 @@ var currentQuestionIndex = 0;
 
 
 //Function to add the question, choices and detect user input
-function addQuestion (currentQ) {
+function addQuestion () {
     //Insert Question from Questions array (questions.js)
-    
 
     var startScreen = document.querySelector('#start-screen');
     var questionsBlk = document.querySelector('#questions');
     var endScreen = document.querySelector('#end-screen');
 
     //var currentQuestion = currentQ[currentQuestionIndex].question
-    var currentQuestion = currentQ.question
+    var currentQuestion = questionsList[currentQuestionIndex]
+    var currentQuestionName = currentQuestion.question
     questionsBlk.innerHTML = `
-        <h2 id="question-title">${currentQuestion}</h2>
+        <h2 id="question-title">${currentQuestionName}</h2>
         <div id="choices" class="choices">
             <ul></ul>
     `;
@@ -48,7 +50,7 @@ function addQuestion (currentQ) {
     //Set readable value to reference ul object
     var questionUL = questionsBlk.querySelector('ul');
     //Set readable value to reference current question options array
-    var optionsArr = currentQ.options;
+    var optionsArr = currentQuestion.options;
 
     //for loop to iter through each element in optionsArr and inset as a list item in the unordered list
     var optionCnt = 1
@@ -60,20 +62,37 @@ function addQuestion (currentQ) {
     showHtml(questionsBlk);
 
     //event handler to detect user intraction with multi-options under questionsBlk (tag with id #questions), taking avantage of event bubbling (Propergation)
-    questionsBlk.addEventListener('click', function(event){
-        var userAnsRaw = event.target.innerText;
-        var userAns = userAnsRaw.substr(3,);
-        var correctAns = currentQ.answer;
+        questionsBlk.addEventListener ('click', function(event) {
+            var userAnsRaw = event.target.innerText;
+            var userAns = userAnsRaw.substr(3,);
+            var correctAns = currentQuestion.answer;
 
             console.log(userAns);
+            console.log(correctAns);
 
-        if (userAns === correctAns){
-            console.log("Correct Answer");
-        } else {
-            console.log ("Wrong Answer");
-        };
-    })
-
+            if (userAns == correctAns){
+                console.log("Correct Answer");
+                showHtml(feedbackBlk);
+                feedbackBlk.insertAdjacentHTML('beforeend', `<h2>CORRECT!!!</h2>`)
+                currentQuestionIndex++;
+                if (currentQuestionIndex < questionsListLgth) {
+                    var h2El = feedbackBlk.querySelector('h2');
+                    console.log('h2El is: ' + h2El);
+                    h2El.innerText('');
+                    addQuestion();
+                }
+            } else {
+                console.log ("Wrong Answer");
+                showHtml(feedbackBlk);
+                feedbackBlk.insertAdjacentHTML('beforeend', `<h2>WRONG!!!</h2>`)
+                currentQuestionIndex++;
+                if (currentQuestionIndex < questionsListLgth) {
+                    var h2El = feedbackBlk.querySelector('h2');
+                    h2El.innerHTML('');
+                    addQuestion();
+                }
+            };
+        })
 };
 
 function hideHtml (htag) {
@@ -116,16 +135,66 @@ startScreenBlk.addEventListener('click', function (event){
         //DEBUG: console.log('Button pressed!!!')
         countDown();
         //while ( currentTime > 0 ) {
-            hideHtml(startScreenBlk);
-            for (let questionNo of questionsList) {
-                //console.log (question);
-                addQuestion(questionNo);
-                setInterval(function () {
-                }, 1000);
-                currentQuestionIndex++;
-            }
-            
-        //}
+        hideHtml(startScreenBlk);
+        
+        addQuestion();
+
+        // Start of Display of Questions    
+        // var notAnswered = true;
+
+        // var startScreen = document.querySelector('#start-screen');
+        // var questionsBlk = document.querySelector('#questions');
+        // var endScreen = document.querySelector('#end-screen');
+
+        // var currentQuestion = questionsList[currentQuestionIndex];
+        // var currentQuestionName = currentQuestion.question;
+
+        // questionsBlk.innerHTML = `
+        //     <h2 id="question-title">${currentQuestionName}</h2>
+        //     <div id="choices" class="choices">
+        //         <ul></ul>
+        // `;
+
+        // //Set readable value to reference ul object
+        // var questionUL = questionsBlk.querySelector('ul');
+        // //Set readable value to reference current question options array
+        // var optionsArr = currentQuestion.options;
+
+        // //for loop to iter through each element in optionsArr and inset as a list item in the unordered list
+        // var optionCnt = 1
+        // for (var option of optionsArr){
+        //     questionUL.insertAdjacentHTML('beforeend', `<button>${optionCnt}. ${option}</button>`);
+        //     optionCnt++;
+        // } 
+
+        // showHtml(questionsBlk);
+
+
+        // //event handler to detect user intraction with multi-options under questionsBlk (tag with id #questions), taking avantage of event bubbling (Propergation)
+        
+        // questionsBlk.addEventListener ('click', function(event) {
+        //     var userAnsRaw = event.target.innerText;
+        //     var userAns = userAnsRaw.substr(3,);
+        //     var correctAns = currentQuestion.answer;
+
+        //     console.log(userAns);
+        //     console.log(correctAns);
+
+        //     if (userAns == correctAns){
+        //         console.log("Correct Answer");
+        //         showHtml(feedbackBlk);
+        //         feedbackBlk.insertAdjacentHTML('afterend', `<h2>CORRECT!!!</h2>`)
+        //         notAnswered = false;
+        
+        //     } else {
+        //         console.log ("Wrong Answer");
+        //         showHtml(feedbackBlk);
+        //         feedbackBlk.insertAdjacentHTML('afterend', `<h2>WRONG!!!</h2>`)
+        //         notAnswered = false;
+        //     };
+        // });
+
+        // currentQuestionIndex++;
     }
 })
 
