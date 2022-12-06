e6f8832aa7b36fc1b7dabc328e3334295c101c49
simon stoneman <simon.stoneman@yahoo.co.uk>
2022-12-06 12:55:57 +0000

    Changed the name of the variable questions in questions.js to questionList as it was getting confusing in the main logic.js. I updated some refs, but missed some to the new var name. Cleaned up some ref to make reference more readable and add comms.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -15,18 +15,24 @@ var questionsBlk = document.querySelector('#questions');
 var endScreen = document.querySelector('#end-screen');
 
 //Insert Question from Questions array (questions.js)
+var currentQuestion = questionsList[currentQuestionIndex].question
 questionsBlk.innerHTML = `
-    <h2 id="question-title">${questions[currentQuestionIndex].question}</h2>
+    <h2 id="question-title">${currentQuestion}</h2>
     <div id="choices" class="choices">
         <ul></ul>
  `;
-var questionUL = questionsBlk.querySelector('ul');
-var optionsArr = questions[currentQuestionIndex].options;
 
+ //Set readable value to reference ul object
+var questionUL = questionsBlk.querySelector('ul');
+//Set readable value to reference current question options array
+var optionsArr = questionsList[currentQuestionIndex].options;
+
+//for loop to iter through each element in optionsArr and inset as a list item in the unordered list
 for (var option of optionsArr){
     questionUL.insertAdjacentHTML('beforeend', `<li>${option}</li>`)
 } 
 
+//event handler to detect user intraction with multi-options under questionsBlk (tag with id #questions), taking avantage of event bubbling (Propergation)
 questionsBlk.addEventListener('click', function(event){
      var userAns = event.target.innerText;
      var correctAns = questionsList[currentQuestionIndex].answer;
