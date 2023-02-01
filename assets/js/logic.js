0fcbd3b9d5b6238253ec4215356ee55a6b685380
SimonStoneman <116112151+SimonStoneman@users.noreply.github.com>
2023-02-01 13:44:31 +0000

    Time deduction for wrong answer was taking too long, so created a new func deductTimer to instantly update the global timerStart and change the timer html element. Removed previous worng answer additions to the countDown func. Added sound to the app for intial page (intro), on pressing 'start' button (start), for right and wrong answers. Created soundOutputnControl func to select the right music to play or pause, based on event e.g. 'play'. Added another event listener on the html block to start the intro music as web browser prevent unsolicited music playing. This fires off playing of into music (but just once). Updated the event listener for the start button to stop the intro 'maintheme' music and start 'letsplay' music.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -18,7 +18,8 @@
 
 ////////////Global Vars Start
 var timerStart = 100;
-var wrongAnsDeduc = 10;
+var stdDeducVal = 1;
+var badansDeducVal = 10;
 var startScreenBlk = document.querySelector('#start-screen');
 var questionsBlk= document.querySelector('#questions');
 var feedbackBlk = document.querySelector('#feedback');
@@ -28,6 +29,23 @@ var currentTime = 0;
 var currentQuestionIndex = 0;
 var currentQuestion = questionsList[currentQuestionIndex];
 var questionUL = '';
+var choicesClickCnt = 0;
+var introClickCnt = 1;
+
+
+//Sounds Vars Start
+
+var soundCnt = 1;
+var sound = '';
+
+const soundsPath = 'assets/sfx/';
+const introSnd = 'maintheme.mp3';
+const startSnd = 'letsplay.mp3';
+const correctAnsSnd = 'correctanswer.mp3';
+const wrongAnsSnd = 'wronganswer.mp3';
+
+//Sounds Vars Start
+
 ////////////Global Vars End
 
 
@@ -38,7 +56,7 @@ var questionUL = '';
 function addQuestion () {
     //Insert Question from Questions array (questions.js)
 
-    console.log (questionsBlk);
+    // console.log (questionsBlk);
     
     currentQuestion = questionsList[currentQuestionIndex];
 
@@ -47,7 +65,8 @@ function addQuestion () {
 
     var currentQuestionName = currentQuestion.question
 
-    console.log (questionsBlkHeader);
+    // console.log (questionsBlkHeader);
+
     //Set inner text to current question name
     questionsBlkHeader.innerText = `${currentQuestionName}`;
 
@@ -56,7 +75,7 @@ function addQuestion () {
     var ulEl = document.createElement('ul')
     var questionChoicesEl = questionsBlk.querySelector('#choices');
 
-    console.log (questionChoicesEl);
+    // console.log (questionChoicesEl);
     
     questionChoicesEl.appendChild(ulEl);
 
@@ -74,73 +93,85 @@ function addQuestion () {
 
     showHtml(questionsBlk);
 
-    //event handler to detect user intraction with multi-options under questionsBlk (tag with id #questions), taking avantage of event bubbling (Propergation)
-        // questionsBlk.addEventListener ('click', function(event) {
-        //     var userAnsRaw = event.target.innerText;
-        //     var userAns = userAnsRaw.substr(3,);
-        //     var correctAns = currentQuestion.answer;
-
-        //     console.log(userAns);
-        //     console.log(correctAns);
-
-        //     if (userAns == correctAns){
-        //         console.log("Correct Answer");
-        //         showHtml(feedbackBlk);
-        //         feedbackBlk.insertAdjacentHTML('beforeend', `<h2>CORRECT!!!</h2>`)
-        //         delay();
-        //         currentQuestionIndex++;
-        //         if (currentQuestionIndex < questionsListLgth) {
-        //             var h2El = feedbackBlk.querySelector('h2');
-        //             console.log('h2El is: ' + h2El);
-        //             removeHtmlEl(h2El);
-        //             removeHtmlEl(questionUL);
-        //         }
-        //     } else {
-        //         console.log ("Wrong Answer");
-        //         showHtml(feedbackBlk);
-        //         feedbackBlk.insertAdjacentHTML('beforeend', `<h2>WRONG!!!</h2>`)
-        //         delay();
-        //         currentQuestionIndex++;
-        //         if (currentQuestionIndex < questionsListLgth) {
-        //             var h2El = feedbackBlk.querySelector('h2');
-        //             removeHtmlEl(h2El);
-        //             removeHtmlEl(questionUL);
-        //         }
-        //     };
-        // })
 };
 
 function hideHtml (htag) {
+
     let htagToHide = htag;
     htagToHide.classList.add('hide')
+
 };
 
 // Changed the class list to remove the static .hide class from the passed in tag
 function showHtml (htag) {
+
     let htagToShow = htag;
     htagToShow.classList.remove('hide')
+
 };
 
 function removeHtmlEl(htag){
+
     var htagToDel = htag;
     htagToDel.remove()
+
 }
 
 function countDown () {
+
     currentTime = setInterval(function () {
         var timeEl = document.querySelector('#time')
         
         timeEl.innerText = timerStart;
-        timerStart = timerStart - wrongAnsDeduc;
 
-        if (timerStart < 0) {
-            clearInterval(currentTime)
+        timerStart = timerStart - stdDeducVal;
+
+        if (timerStart == 0) {
+            window.location = './highscores.html';
+            clearInterval(currentTime);
         }
     }, 1000);
+
 };
 
-function delay (){
-    setInterval(function () {},1000);
+function deductTimer () {
+
+        var timeEl = document.querySelector('#time')
+
+        timerStart = timerStart - badansDeducVal;
+
+        timeEl.innerText = timerStart;
+        
+};
+
+function soundOutputnControl(action, type) {
+
+    switch (type) {
+        case 'intro':
+                        if (soundCnt === 1) {
+                            console.log(`first play ${type}`)
+                            sound = new Audio(soundsPath + introSnd);
+                            soundCnt++
+                        }
+                        break;
+        case 'start':
+                        sound = new Audio(soundsPath + startSnd);
+                        break;
+        case 'correct':
+                        sound = new Audio(soundsPath + correctAnsSnd);
+                        break;
+        case 'wrong':
+                        sound = new Audio(soundsPath + wrongAnsSnd);
+                        break;
+    };
+
+    if (action === 'play') {
+        console.log(`play audio (${type})`);
+        sound.play();
+    } else {
+        console.log(`pause audio (${type})`);
+        sound.pause();
+    };
 };
 
 //Functions End
@@ -153,78 +184,113 @@ var htmlTimerVal = document.querySelector('#time');
 htmlTimerVal.innerText = timerStart;
 currentTime = timerStart;
 
+document.querySelector('html').addEventListener('click', function() {
+    console.log(introClickCnt);
+    if (introClickCnt === 1) {
+        soundOutputnControl('play', 'intro');
+        introClickCnt++;
+    }
+});
 
+// Action a click event in the startscreenblk for start button
 startScreenBlk.addEventListener('click', function (event){
     buttonPress = event.target.tagName;
     //DEBUG: console.log('Element selected is: ' + buttonPress);
 
-    console.log('currentQuestionIndex in startblk: ' + currentQuestionIndex);
+    // console.log('currentQuestionIndex in startblk: ' + currentQuestionIndex);
 
     if (buttonPress === 'BUTTON'){
         //DEBUG: console.log('Button pressed!!!')
+        soundOutputnControl('stop', 'intro');
+        soundOutputnControl('play', 'start');
         countDown();
         //while ( currentTime > 0 ) {
         hideHtml(startScreenBlk);
         addQuestion();
-    }
-})
+    };
+});
 
-console.log (questionsBlk);
+// console.log (questionsBlk);
 
-questionsBlk.addEventListener ('click', function(event) {
+questionsBlk.querySelector('#choices').addEventListener ('click', function(event) {
         var userAnsRaw = event.target.innerText;
         var userAns = userAnsRaw.substr(3,);
         var correctAns = currentQuestion.answer;
 
-        console.log(userAns);
-        console.log(correctAns);
-        console.log('currentQuestionIndex in qblk: ' + currentQuestionIndex);
-
-        // Check if the users answer matches the correct answer from current question
-        if (userAns == correctAns){
-            console.log("Correct Answer");
-            // Show the feedback block (tag) below the list of answers, by removing the .hide from the class list
-            showHtml(feedbackBlk);
-            // Add a new html block (for the result) below the feedback div tag
-            feedbackBlk.insertAdjacentHTML('beforeend', `<h2>CORRECT!!!</h2>`)
-            // delay();
-            currentQuestionIndex++;
-            // if (currentQuestionIndex < questionsListLgth) {
-            //     var h2El = feedbackBlk.querySelector('h2');
-            //     console.log('h2El is: ' + h2El);
-            //     removeHtmlEl(h2El);
-            //     removeHtmlEl(questionUL);
-            //     addQuestion();
-            // }
-            
-        } else {
-            console.log ("Wrong Answer");
-            showHtml(feedbackBlk);
-            feedbackBlk.insertAdjacentHTML('beforeend', `<h2>WRONG!!!</h2>`)
-            // delay();
-            currentQuestionIndex++;
-            // if (currentQuestionIndex < questionsListLgth) {
-            //     var h2El = feedbackBlk.querySelector('h2');
-            //     removeHtmlEl(h2El);
-            //     removeHtmlEl(questionUL);
-            //     addQuestion();
-            // }
+        // console.log(userAns);
+        // console.log(correctAns);
+        // console.log('currentQuestionIndex in qblk: ' + currentQuestionIndex);
+        if (choicesClickCnt === 0) {
+            choicesClickCnt = 1;
+        } else{
+            choicesClickCnt++;
         };
+        
+        console.log(`choicesClickCnt value on initalising: ${choicesClickCnt}`);
 
-        // use a timeout func to wrap around items that clear the screen ready for the new question
-        setTimeout(()=> {
-    
-            if (currentQuestionIndex < questionsListLgth) {
-                var h2El = feedbackBlk.querySelector('h2');
-                removeHtmlEl(h2El);
-                removeHtmlEl(questionUL);
-                addQuestion();
-            }
+        if (choicesClickCnt === 1) {
 
-        }, 1000);
+            console.log(`Passed through choicesClickCnt check, ${choicesClickCnt}`);
 
-    })
 
+            // Check if the users answer matches the correct answer from current question
+            if (userAns == correctAns){
+
+                soundOutputnControl('play', 'correct');
+
+                console.log("Correct Answer");
+                // Show the feedback block (tag) below the list of answers, by removing the .hide from the class list
+                showHtml(feedbackBlk);
+                // Add a new html block (for the result) below the feedback div tag
+                feedbackBlk.insertAdjacentHTML('beforeend', `<h2>CORRECT!!!</h2>`)
+  
+                currentQuestionIndex++;
+                
+            } else {
+                
+                // console.log ("Wrong Answer");
+
+                // As the wrong answer has been selected we need to create a penalty by deducting the time count by 10 seconds
+                deductTimer()
+
+                soundOutputnControl('play', 'wrong');
+
+                // Display the feedback area
+                showHtml(feedbackBlk);
+
+                // Add the feedback for the user selected answer to the feedback block
+                feedbackBlk.insertAdjacentHTML('beforeend', `<h2>WRONG!!!</h2>`)
+                
+                // Increase the question index to move to the next question and answer data
+                currentQuestionIndex++;
+
+            };
+
+            // use a timeout func to wrap around items that clear the screen ready for the new question
+            setTimeout(()=> {
+        
+                if (currentQuestionIndex < questionsListLgth) {
+                    
+                    // Create short target for selection of h2 tag under feedbackblock html section
+                    var h2El = feedbackBlk.querySelector('h2');
+                    
+                    // Screen clear down function calls and add new question title
+                        // Remove the h2 element from the feedback area
+                        removeHtmlEl(h2El);
+                        // Remove the choices elements from the choices area
+                        removeHtmlEl(questionUL);
+                        // Add the new question title to the page
+                        addQuestion();
+                    
+                } else {
+                    // as we have got to the end of the questions, we need to change the window object location to the highscores page
+                    window.location = './highscores.html'
+                };
+
+                choicesClickCnt = 0;
+            }, 500);
+        };
+});
 
 
  
